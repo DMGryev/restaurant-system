3e42b7e9d1f45a55fa96fc1597d3839f87328693
DMGryev <newdim2005@mail.ru>
2026-05-22 16:14:14 +0700

    fix:в /auth/me

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -88,4 +88,4 @@ async def register_initial(data: UserCreate, db: AsyncSession = Depends(get_db))
 
 @router.get("/me", response_model=UserResponse)
 async def get_me(current_user: User = Depends(get_current_user)):
-    return UserResponse.model_validate(current_user)
+    return UserResponse.model_validate(current_user) #опечатка у меня тута
